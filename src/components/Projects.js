2700bc68fcfa3ea84cfb98ac74d6c1b0699a5dd4
ndrewbarz <ndrewbarz@gmail.com>
2020-06-29 10:32:05 +0300

    data.js file added & changes in project.js file

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,60 +1,32 @@
-class Projects {
-	constructor(title, desc, codeLink, demoLink) {
-		this.title = title;
-		this.desc = desc;
-		this.codeLink = codeLink;
-		this.demoLink = demoLink;
-	}
+let template = ``;
 
-}
+const projectBox = document.querySelector('.project-box');
 
-export class ProjectFactory {
-  create(title, desc, codeLink, demoLink, ...tags) {
-    // Project box
-    const box = document.querySelector(".project-box");
-    // Project
-    const project = createEl("div", "project");
-    box.append(project);
-    const header = createEl("div", "project-title");
-    // Title
-    const h1 = createEl("h1");
-    h1.innerHTML = `${title}`;
-    header.append(h1);
-    // Description
-    const p = createEl("p");
-    p.innerHTML = `${desc}`;
-    // Tags
-    const tagBox = createEl("div", "tags");
-    // Links
-    const linkBox = createEl("div", "links");
-    const linkCode = createEl("a", "code", "Source");
-		const linkDemo = createEl("a", "demo", "View");
-		
-    setAttributes(linkCode, { href: `${codeLink}`, target: "_blank" });
-    setAttributes(linkDemo, { href: `${demoLink}`, target: "_blank" });
-
-    linkBox.append(linkCode, linkDemo);
-		project.append(header, p, tagBox, linkBox);
-		
-		// Tags
-    tags.forEach(t => {
-      const tagEl = createEl("span", "tag", `${t}`);
-      tagBox.append(tagEl);
-		});
-		
-    return new Projects();
-  }
-}
-
-function createEl (elm, className, text) {
-	const el = document.createElement(elm);
-				if (className) el.classList.add(className);
-				if (text) el.textContent = text;
-	return el;
-}
-
-function setAttributes(el, attrs) {
-  for(var key in attrs) {
-    el.setAttribute(key, attrs[key]);
-  }
-}
+// Display projects
+export const projectsList = (arr) => {
+  return arr.forEach((el) => {
+    // const tags = el.tags.map((el) => el);
+    const tags = el.tags.join(', ');
+    template = `
+    <div class="project">
+    
+      <div class="project-title">
+        <h1>${el.name}</h1>
+      </div>
+      
+      <p>${el.desc}</p>
+      
+      <div class="tags">
+        <span class="tag">${tags}</span>
+      </div>
+  
+      <div class="links">
+        <a class="code" href="${el.codeLink}" target="_blank">Source</a>
+        <a class="demo" href="${el.demoLink}" target="_blank">View</a>
+      </div>
+      
+    </div>
+  `;
+    projectBox.insertAdjacentHTML('afterbegin', template);
+  });
+};
---
+++ src/components/data.js
@@ -0,0 +1,35 @@
+// Projects data
+export const data = [
+  {
+    name: 'GitHub Search App',
+    desc:
+      'Search GitHub users by username, display profile info. Using async/await and Fetch API to get the user from GitHub API.',
+    codeLink: 'https://github.com/ndrewbarz/searchapp',
+    demoLink: 'https://ndrewbarz.github.io/searchapp/',
+    tags: ['bootstrap', 'js', 'fetch', 'async/await', 'api'],
+  },
+  {
+    name: 'Chuck Norris',
+    desc:
+      'A simple app that uses a third-party API to display Chuck Norris jokes, interacts with the API using asynchronous functions and a Fetch API',
+    codeLink: 'https://github.com/ndrewbarz/chuck-norris-joke',
+    demoLink: 'https://ndrewbarz.github.io/chuck-norris-joke/',
+    tags: ['js', 'fetch', 'api'],
+  },
+  {
+    name: 'Weather App',
+    desc:
+      'Show current weather, you can change the location. Created by openweathermap API, using async/await and Fetch API. Stored data to LocalStorage',
+    codeLink: 'https://github.com/ndrewbarz/weather-app',
+    demoLink: 'https://ndrewbarz.github.io/weather-app/',
+    tags: ['bootstrap', 'js', 'fetch', 'async/await', 'api'],
+  },
+  {
+    name: 'Custom HTTP Lib',
+    desc:
+      "Library for making HTTP requests. You can make GET, POST, PUT and DELETE request's",
+    codeLink: 'https://github.com/ndrewbarz/customHTTPLibrary',
+    demoLink: 'https://github.com/ndrewbarz/customHTTPLibrary',
+    tags: ['js', 'fetch', 'async/await'],
+  },
+];
